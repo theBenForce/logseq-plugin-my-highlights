84d03af46ce14697af3885d9456781d1df2e492c
Ben Force <benforce@gmail.com>
2022-03-03 15:58:38 -0600

    fix: add ua tag

--- vite.config.ts
+++ vite.config.ts
@@ -24,13 +24,15 @@ const devIndexHtmlPlugin: () => Plugin = () => {
       <!DOCTYPE html>
       <html lang="en">
         <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NVFJD2S');</script>
-          <!-- End Google Tag Manager -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WC2KDBBYJJ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WC2KDBBYJJ');
+        </script>
           <base href="http://${config.server.host}:${config.server.port}">
           <meta charset="UTF-8" />
           <script type="module" src="/@vite/client"></script>
@@ -46,10 +48,6 @@ const devIndexHtmlPlugin: () => Plugin = () => {
           <title>React Plugin</title>
         </head>
         <body>
-        <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVFJD2S"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-      <!-- End Google Tag Manager (noscript) -->
           <div id="app"></div>
           <script type="module" src="/src/main.tsx"></script>
         </body>
