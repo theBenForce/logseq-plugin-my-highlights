5fde7bc94991157a0e6eecd66140fc15ba8880f3
Ben Force <benforce@gmail.com>
2022-03-03 10:52:44 -0600

    feat: create sentry release

---
+++ createRelease.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export NEXT_VERSION=$1
+
+echo "Build Plugin"
+echo VERSION=$NEXT_VERSION > .env.production.local
+pnpm build
+
+echo "Create Sentry Release"
+npx sentry-cli releases new $NEXT_VERSION -e prod
+npx sentry-cli releases set-commits --auto $NEXT_VERSION
+npx sentry-cli releases finalize $NEXT_VERSION
+
+echo "Create Build Artifact"
+npx zip -qq -r logseq-plugin-my-highlights-$NEXT_VERSION.zip dist docs icon.svg readme.md LICENSE package.json
