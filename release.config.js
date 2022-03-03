8788eab6f4309a89f34b0303fef6e34149db51d5
Ben Force <benforce@gmail.com>
2022-03-03 11:31:25 -0600

    feat: create env for sentry config

--- release.config.js
+++ release.config.js
@@ -1,3 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const SENTRY_AUTH_TOKEN = process.env.SENTRY_AUTH_TOKEN;
+const SENTRY_ORG = process.env.SENTRY_ORG;
+const SENTRY_PROJECT = process.env.SENTRY_PROJECT;
+const SENTRY_DSN = process.env.SENTRY_DSN;
+
+fs.writeFileSync(path.resolve(process.cwd(), '.env'), `
+SENTRY_AUTH_TOKEN=${SENTRY_AUTH_TOKEN}
+SENTRY_ORG=${SENTRY_ORG}
+SENTRY_PROJECT=${SENTRY_PROJECT}
+SENTRY_DSN=${SENTRY_DSN}
+SENTRY_LOG_LEVEL=info`, 'utf-8');
+
 module.exports = {
   branches: ["master"],
   plugins: [
@@ -20,7 +35,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "./createRelease.sh ${nextRelease.version}",
+          `./createRelease.sh ${nextRelease.version}`,
       },
     ],
     [
