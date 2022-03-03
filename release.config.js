c4df5b205f3156149c207074c7232f8205cd4b43
Ben Force <benforce@gmail.com>
2022-03-03 10:52:44 -0600

    feat: create sentry release

--- release.config.js
+++ release.config.js
@@ -20,14 +20,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "echo \"VERSION=${nextRelease.version}\" > .env.production.local && pnpm build",
-      },
-    ],
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd:
-          "zip -qq -r logseq-plugin-my-highlights-${nextRelease.version}.zip dist docs icon.svg readme.md LICENSE package.json",
+          "./createRelease.sh ${nextRelease.version}",
       },
     ],
     [
