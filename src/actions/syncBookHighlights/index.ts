d038cdcc7231f22edd82a4ef8fd96e376e8f01ef
Ben Force <benforce@gmail.com>
2023-01-06 01:22:01 -0600

    feat: add highlights to journal pages

--- src/actions/syncBookHighlights/index.ts
+++ src/actions/syncBookHighlights/index.ts
@@ -1,9 +1,9 @@
 
 import { ILSPluginUser } from '@logseq/libs/dist/LSPlugin.user';
 import * as Sentry from '@sentry/react';
-import { assert } from 'console';
 import { getBookPage } from '../../hooks/useImportBooks';
 import { KindleBook } from '../../utils/parseKindleHighlights';
+import { createJournalBlocks } from './createJournalBlocks';
 import { createPageBlocks } from './createPageBlocks';
 import { updateBookDetails } from './updateBookDetails';
 
@@ -20,20 +20,24 @@ export const syncBookHighlights = async ({book, logseq}: SyncBookHighlightsParam
     const { page, pageBlocksTree } = await getBookPage({ logseq, book });
     const firstBlock = pageBlocksTree[0];
 
-    await logseq.Editor.upsertBlockProperty(firstBlock.uuid, "last_sync", new Date().toISOString());
-    await updateBookDetails({
-      book, uuid: firstBlock.uuid, editor: logseq.Editor
-    });
-
-    assert(page !== null, 'Could not find page');
+    if(page === null) throw new Error('Could not find page');
 
     if (logseq.settings?.highlight_location === "journal") {
-      throw new Error('Journal import not implemented!');
+      await createJournalBlocks({
+        page: page!,
+        book,
+        logseq
+      })
     } else {
       await createPageBlocks({
         page: page!, pageBlocksTree, book, logseq
       });
     }
+
+    await logseq.Editor.upsertBlockProperty(firstBlock.uuid, "last_sync", new Date().toISOString());
+    await updateBookDetails({
+      book, uuid: firstBlock.uuid, editor: logseq.Editor
+    });
 
     console.info(`Done importing ${book.title}`);
   } catch (ex) {
